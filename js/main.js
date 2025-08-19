647374109720227a69a15ee1a6641871ca31c076
jacobluckiewicz <jakub.luckiewicz1@gmail.com>
2025-08-19 22:08:44 +0200

    fixing bugs in about-me

--- js/main.js
+++ js/main.js
@@ -1,35 +1,31 @@
+//  this is changing a border color of element
+const boxes = document.querySelectorAll('.wrap-container')
 
-//  this is changing a border color of element 
-const boxes = document.querySelectorAll('.wrap-container');
+const observer = new IntersectionObserver(
+	entries => {
+		entries.forEach(entry => {
+			if (entry.isIntersecting) {
+				entry.target.classList.add('visible')
+			} else {
+				entry.target.classList.remove('visible')
+			}
+		})
+	},
+	{ threshold: 0.7 }
+) // 70% must be viewed
 
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add('visible');
-      } else {
-        entry.target.classList.remove('visible');
-      }
-    });
-  }, { threshold: 0.7 }); // 70% must be viewed
+boxes.forEach(box => observer.observe(box))
 
-  boxes.forEach(box => observer.observe(box));
+// gallery photo for quick facts
+const images = ['images/idk_how_to_name_it.jpg', 'images/sheep-landscape.jpg', 'images/kjeragbolten.jpg']
 
-
-  // gallery photo for quick facts 
-  const images = [
-  "https://github.com/jacobluckiewicz/portfolio/blob/a34664ab03ff7e7a0d60da5bc1d2fe462bde8b80/images/IMG_20250810_135300.jpg",
-  "https://github.com/jacobluckiewicz/portfolio/blob/main/images/kjeragbolten.jpg",
-  "https://github.com/jacobluckiewicz/portfolio/blob/main/images/sheep-landscape.jpg",
-  "https://github.com/jacobluckiewicz/portfolio/blob/main/images/idk_how_to_name_it.jpg"
-];
-
-let index = 0;
-const gallery = document.getElementById("shelf");
+let index = 0
+const gallery = document.getElementById('shelf')
 
 function changeBackground() {
-  gallery.style.backgroundImage = `url(${images[index]})`;
-  index = (index + 1) % images.length;
+	gallery.style.backgroundImage = `url(${images[index]})`
+	index = (index + 1) % images.length
 }
 
-changeBackground(); // show first image
-setInterval(changeBackground, 4000); // change every 4s
+changeBackground() // show first image
+setInterval(changeBackground, 4000) // change every 4s
