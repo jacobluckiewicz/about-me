1e456dd37050290a7c57aed8141a1bb9dfa755c6
jacobluckiewicz <jakub.luckiewicz1@gmail.com>
2025-08-19 21:14:07 +0200

    adding section about-me

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+
+//  this is changing a border color of element 
 const boxes = document.querySelectorAll('.wrap-container');
 
   const observer = new IntersectionObserver((entries) => {
@@ -8,6 +10,26 @@ const boxes = document.querySelectorAll('.wrap-container');
         entry.target.classList.remove('visible');
       }
     });
-  }, { threshold: 0.7 }); // 70% elementu musi być widoczne
+  }, { threshold: 0.7 }); // 70% must be viewed
 
   boxes.forEach(box => observer.observe(box));
+
+
+  // gallery photo for quick facts 
+  const images = [
+  "https://github.com/jacobluckiewicz/portfolio/blob/a34664ab03ff7e7a0d60da5bc1d2fe462bde8b80/images/IMG_20250810_135300.jpg",
+  "https://github.com/jacobluckiewicz/portfolio/blob/main/images/kjeragbolten.jpg",
+  "https://github.com/jacobluckiewicz/portfolio/blob/main/images/sheep-landscape.jpg",
+  "https://github.com/jacobluckiewicz/portfolio/blob/main/images/idk_how_to_name_it.jpg"
+];
+
+let index = 0;
+const gallery = document.getElementById("shelf");
+
+function changeBackground() {
+  gallery.style.backgroundImage = `url(${images[index]})`;
+  index = (index + 1) % images.length;
+}
+
+changeBackground(); // show first image
+setInterval(changeBackground, 4000); // change every 4s
